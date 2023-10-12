c055bfb62092b97625af285b92c1b077cc9a48f9
tony-yun <lifeyun24@naver.com>
2023-10-12 23:29:53 +0900

    diffable datasource, snapshot, compositional layout setting

--- SpotifyPaywall/PaywallViewController.swift
+++ SpotifyPaywall/PaywallViewController.swift
@@ -20,15 +20,68 @@ class PaywallViewController: UIViewController {
     // 배너 별로 쓰일 배경색상
     let colors: [UIColor] = [.systemPurple, .systemOrange, .systemPink, .systemRed]
     
-    
+    typealias Item = BannerInfo
+    enum Section {
+        case main
+    }
+       
+    var datasource: UICollectionViewDiffableDataSource<Section, Item>!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+//        if let flowlayout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+//            flowlayout.estimatedItemSize = .zero
+//        }
+        
+        // page control의 점을 총 몇개 보여줄 것인지
+        pageControl.numberOfPages = bannerInfos.count
+        
         // presentation: diffable datasource
+        datasource = UICollectionViewDiffableDataSource<Section, Item>(collectionView: collectionView, cellProvider: { collectionView, indexPath, itemIdentifier in
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "BannerCell", for: indexPath) as? BannerCell else {
+                return nil
+            }
+            cell.configure(itemIdentifier)
+            
+            // 배경색 설정
+            cell.backgroundColor = self.colors[indexPath.item]
+            return cell
+        })
+        
         // data: snapshot
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(bannerInfos, toSection: .main)
+        datasource.apply(snapshot)
+        
         // layout: compositional layout
+        collectionView.collectionViewLayout = layout()
         
+    }
+    private func layout() -> UICollectionViewCompositionalLayout {
         
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.8), heightDimension: .absolute(200))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        let section = NSCollectionLayoutSection(group: group)
+        
+        // 방향을 수평으로 설정하기, 
+        // .continuous는 스르륵 스크롤, .groupPaging은 왼쪽에 맞춰서 촥촥 스크롤
+        section.orthogonalScrollingBehavior = .groupPagingCentered
+        section.interGroupSpacing = 20
+        
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
+    }
+}
+
+// page control 설정
+extension PaywallViewController: UIScrollViewDelegate {
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let index = Int(scrollView.contentOffset.x / self.collectionView.bounds.width)
+        pageControl.currentPage = index
     }
 }
